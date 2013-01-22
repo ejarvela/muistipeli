a0e3e4700b7479058f85055d0e8faaf90771882d
Emilia Järvelä <emilia.jarvela@helsinki.fi>
2013-01-22 16:36:37 +0200

    luokkakaavio lisatty, koodia aloitettu, testeja lisatty

--- Muistipeli/src/muistipeli/Muistipeli.java
+++ Muistipeli/src/muistipeli/Muistipeli.java
@@ -1,20 +1,63 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package muistipeli;
 
-/**
- *
- * @author Emilia
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Muistipeli {
 
-    
- 
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-           System.out.println("Hello world");
+    private Scanner lukija;
+    ArrayList<Pelaaja> pelaajat;
+
+    public Muistipeli(Scanner lukija) {
+        this.lukija = lukija;
+        pelaajat = new ArrayList<Pelaaja>();
     }
-}
+
+    public void kaynnista() {
+        System.out.println("Tervetuloa Muistipeliin!");
+        System.out.println("");
+        System.out.println("Montako pelaajaa? (1-4)");
+        
+        int pelaajienMaara;
+        
+        pelaajienMaara = Integer.parseInt(lukija.nextLine());
+        
+        while(pelaajienMaara > 4 || pelaajienMaara < 1) {
+        System.out.println("Anna pelaajien lukumäärä väliltä 1-4.");
+        pelaajienMaara = Integer.parseInt(lukija.nextLine());
+        }
+        
+        System.out.println("Anna pelaajien nimet yksi kerrallaan.");
+        System.out.println("");
+         
+        String nimi;
+
+        for(int i = 0; i < pelaajienMaara; i++){
+            System.out.println("Osallistujan nimi:");
+            nimi = lukija.nextLine();
+                pelaajat.add(new Pelaaja(nimi));
+            }
+        
+        System.out.println("Valitse vaikeustaso.");
+        System.out.println("1 - 4x4");
+        System.out.println("2 - 6x6");
+        System.out.println("3 - 8x8");
+        
+        int vaikeus;
+        vaikeus = Integer.parseInt(lukija.nextLine());
+        
+        while(vaikeus > 3 || vaikeus < 1) {
+        System.out.println("Valitse vaikeustaso 1, 2 tai 3.");
+        vaikeus = Integer.parseInt(lukija.nextLine());
+        }
+        
+        System.out.println("");
+        System.out.println("Peli alkaa.");
+        
+        }
+    }
+
+
+    
+    
+
